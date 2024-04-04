2e1beefeef080aaa82152e80e73b8cf37f548a04
Quang Luong <vquangluong@gmail.com>
2024-04-04 15:42:57 +0700

    feat(#setting): add setting display notify button for product list page and product detail page

--- Block/Catalog/Product/View.php
+++ Block/Catalog/Product/View.php
@@ -12,6 +12,13 @@ class View extends \Magento\ProductAlert\Block\Product\View
 {
     protected $_template = 'catalog/product/view.phtml';
 
+    public function setNameInLayout($name)
+    {
+        // Delete default alert stock
+        $this->getLayout()->unsetElement("productalert.stock");
+        return parent::setNameInLayout($name);
+    }
+
     public function getProduct()
     {
         return parent::getProduct();
